86a307b8126e9ed3027f0d1aecded68bbed5317a
IceBlink <lyutiko.alex@gmail.com>
2019-05-12 15:41:46 +0300

    Добавьте файлы проекта.

---
+++ RegressionLib/CubicRegression.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegressionLib
+{
+    /// <summary>
+    /// Cubic regression model
+    /// </summary>
+    [Serializable]
+    public class CubicRegression : AbstractRegression
+    {
+        /// <summary>
+        /// Regression coefficients
+        /// </summary>
+        private double xCubicCoef, xSqCoef, xCoef, freeCoef;
+        /// <summary>
+        /// Calculates regression coefficients, approximation error and correlation coefficient
+        /// </summary>
+        /// <param name="xVals"></param>
+        /// <param name="yVals"></param>
+        public CubicRegression(double[] xVals, double[] yVals) : base(xVals, yVals)
+        {
+            double[,] matrix = new double[4, 4];
+            double[] vector = new double[4];
+            matrix[0, 0] = GetMultipliedSum(new List<double[]>() { xVals, xVals, xVals });
+            matrix[0, 1] = GetMultipliedSum(new List<double[]>() { xVals, xVals });
+            matrix[0, 2] = xVals.Sum();
+            matrix[0, 3] = xVals.Length;
+            matrix[1, 0] = GetMultipliedSum(new List<double[]>() { xVals, xVals, xVals, xVals });
+            matrix[1, 1] = matrix[0, 0];
+            matrix[1, 2] = matrix[0, 1];
+            matrix[1, 3] = matrix[0, 2];
+            matrix[2, 0] = GetMultipliedSum(new List<double[]>() { xVals, xVals, xVals, xVals, xVals });
+            matrix[2, 1] = matrix[1, 0];
+            matrix[2, 2] = matrix[1, 1];
+            matrix[2, 3] = matrix[1, 2];
+            matrix[3, 0] = GetMultipliedSum(new List<double[]>() { xVals, xVals, xVals, xVals, xVals, xVals });
+            matrix[3, 1] = matrix[2, 0];
+            matrix[3, 2] = matrix[2, 1];
+            matrix[3, 3] = matrix[2, 2];
+            vector[0] = yVals.Sum();
+            vector[1] = GetMultipliedSum(new List<double[]>() { xVals, yVals });
+            vector[2] = GetMultipliedSum(new List<double[]>() { xVals, xVals, yVals });
+            vector[3] = GetMultipliedSum(new List<double[]>() { xVals, xVals, xVals, yVals });
+            System = new SystemOfLinearEquations(matrix, vector);
+            List<double> solvation = System.Solve();
+            xCubicCoef = solvation[0];
+            xSqCoef = solvation[1];
+            xCoef = solvation[2];
+            freeCoef = solvation[3];
+            correlationCoefficient = GetCorrelationCoefficient();
+            approximationError = GetError();
+        }
+        /// <summary>
+        /// A vector of regression coefficient is got by solving this system
+        /// </summary>
+        public SystemOfLinearEquations System { get; set; }
+        /// <summary>
+        /// Overrides PredictY method from AbstractRegression with a1*x^3 + a2*x^2 + a3*x + b
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public override double PredictY(double x)
+        {
+            return xCubicCoef * x * x * x + xSqCoef * x * x + xCoef * x + freeCoef;
+        }
+        /// <summary>
+        /// String representation
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"{xCubicCoef:f5} * x^3 + {xSqCoef:f5} * x^2 + {xCoef:f5} * x + {freeCoef:f5}";
+        }
+    }
+}
---
+++ RegressionLib/Exponential1Regression.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegressionLib
+{
+    public class Exponential1Regression : ExponentialRegression
+    {
+        public Exponential1Regression(double[] xVals, double[] yVals) : base(xVals, yVals)
+        {}
+
+        protected override double GetXCoefficient()
+        {
+            return Math.Exp(base.GetXCoefficient());
+        }
+        protected override double GetFreeCoefficient()
+        {
+            return Math.Exp((yValsConverted.Sum() - Math.Log(xCoefficient) * xValsConverted.Sum()) / xValsConverted.Length);
+        }
+        public override string ToString()
+        {
+            return $"{a0:f5} * {xCoefficient:f5}^x";
+        }
+    }
+}
